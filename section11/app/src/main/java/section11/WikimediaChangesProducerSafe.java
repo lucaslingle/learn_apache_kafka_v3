5745a70a3b255845983fcdaa8c11f8f8470945b5
lucaslingle <lucasdaxlingle@gmail.com>
2026-06-23 14:42:30 -0700

    section11 - added WikimediaChangesProducerSafe

--- section11/app/src/main/java/section11/WikimediaChangesProducerSafe.java
+++ section11/app/src/main/java/section11/WikimediaChangesProducerSafe.java
@@ -1,4 +1,4 @@
-// lecture 61
+// lecture 68
 package section11;
 
 import java.net.URI;
@@ -11,13 +11,21 @@ import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
 import okhttp3.Headers;
 
-public class WikimediaChangesProducer {
+public class WikimediaChangesProducerSafe {
     public static void main(String[] args) throws InterruptedException {
         String bootstrapServers = "127.0.0.1:9092";
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        // using the defaults in kafka 3.0+ but setting them here explicitly below.
+        // somehow should set broker-side min.insync.replicas = 2, 
+        // but not here since this is producer only.
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5"); // > 5 breaks idempotence support
+        properties.setProperty(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "120000");
 
         String topic = "wikimedia.recentchange";
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
